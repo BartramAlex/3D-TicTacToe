44d03d0fd5df98f052928a1ee490e9a88e8c1464
thehollowoak <thehollowoak@gmail.com>
2016-11-19 22:44:56 -0500

    Update AI.hpp
    added priorities 3 and 4

--- AI.hpp
+++ AI.hpp
@@ -1,8 +1,9 @@
 
 // File for all the AI decision functions
 
-bool checkPriority12(int winner, int x, int y, int mainBoard[3][3][3]) { // returns true if priority is 1, false if not
+bool checkPriority12(int winner, int x, int y, int mainBoard[3][3][3]) { // returns true if priority is 1 or 2, false if not
 
+    // copy main board over, so we can change it without changing the main board
     int board[3][3][3];
     for (int i=2; i>-1; i--) {
         for (int j=0; j<3; j++) {
@@ -12,10 +13,12 @@ bool checkPriority12(int winner, int x, int y, int mainBoard[3][3][3]) { // retu
         }
     }
 
+    // set z and place move on the board
     int z;
     if (board[x][y][0] == 0) {z = 0;}
     else if (board[x][y][1] == 0) {z = 1;}
-    else {z = 2;}
+    else if (board[x][y][2] == 0) {z = 2;}
+    else {return false;} // spot is full, can't play here
     board[x][y][z] = winner;
 
     int in_a_row = 0;
@@ -33,7 +36,7 @@ bool checkPriority12(int winner, int x, int y, int mainBoard[3][3][3]) { // retu
     }
     if (in_a_row == 3) return true;
     in_a_row = 0;
-    // check win in y direction
+    // check win in z direction
     for (int i = 0; i < 3; i++) {
         if (board[x][y][i] == winner)
             ++in_a_row;
@@ -124,4 +127,160 @@ bool checkPriority12(int winner, int x, int y, int mainBoard[3][3][3]) { // retu
         if (in_a_row == 3) return true;
     }
     return false;
+}
+
+int checkPriority34(int x, int y, int mainBoard[3][3][3]) { // returns number of set-ups for future wins a play makes, 0-2
+
+    // copy main board over, so we can change it without changing the main board
+    int board[3][3][3];
+    for (int i=2; i>-1; i--) {
+        for (int j=0; j<3; j++) {
+            for (int k=0; k<3; k++) {
+                board[i][j][k] = mainBoard[i][j][k];
+            }
+        }
+    }
+
+    // set z and place move on the board
+    int z;
+    if (board[x][y][0] == 0) {z = 0;}
+    else if (board[x][y][1] == 0) {z = 1;}
+    else if (board[x][y][2] == 0) {z = 2;}
+    else {return 0;} // spot is full, can't play here
+    board[x][y][z] = 2;
+
+    int in_a_row = 0;
+    int emptySpot = 0;
+    int set_ups = 0;
+    // check set-up in x direction
+    for (int i = 0; i < 3; i++) {
+        if (board[i][y][z] == 2)
+            ++in_a_row;
+        else if (board[i][y][z] == 0)
+            ++emptySpot;
+    }
+    if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+    // check set-up in y direction
+    in_a_row = emptySpot = 0;
+    for (int i = 0; i < 3; i++) {
+        if (board[x][i][z] == 2)
+            ++in_a_row;
+        else if (board[x][i][z] == 0)
+            ++emptySpot;
+    }
+    if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+    in_a_row = emptySpot = 0;
+    // check set-up in z direction
+    for (int i = 0; i < 3; i++) {
+        if (board[x][y][i] == 2)
+            ++in_a_row;
+        else if (board[x][y][i] == 0)
+            ++emptySpot;
+    }
+    if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+    in_a_row = emptySpot = 0;
+    // check set-up in xy diagonals if spot is on that diagonal
+    if (x == y) { // if on first diagonal
+        for (int i = 0; i < 3; i++) {
+            if (board[i][i][z] == 2)
+                ++in_a_row;
+            else if (board[i][i][z] == 0)
+                ++emptySpot;
+        }
+        if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+        in_a_row = emptySpot = 0;
+    }
+    if (x + y == 2) { // if on the second diagonal
+        for (int i = 0; i < 3; i++) {
+            if (board[i][2-i][z] == 2)
+                ++in_a_row;
+            else if (board[i][2-i][z] == 0)
+                ++emptySpot;
+        }
+        if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+        in_a_row = emptySpot = 0;
+    }
+    // check set-up in xz diagonals if spot is on that diagonal
+    if (x == z) { // if on first diagonal
+        for (int i = 0; i < 3; i++) {
+            if (board[i][y][i] == 2)
+                ++in_a_row;
+            else if (board[i][y][i] == 0)
+                ++emptySpot;
+        }
+        if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+        in_a_row = emptySpot = 0;
+    }
+    if (x + z == 2) { // if on the second diagonal
+        for (int i = 0; i < 3; i++) {
+            if (board[i][y][2-i] == 2)
+                ++in_a_row;
+            else if (board[i][y][2-i] == 0)
+                ++emptySpot;
+        }
+        if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+        in_a_row = emptySpot = 0;
+    }
+    // check set-up in yz diagonals if spot is on that diagonal
+    if (y == z) { // if on first diagonal
+        for (int i = 0; i < 3; i++) {
+            if (board[x][i][i] == 2)
+                ++in_a_row;
+            else if (board[x][i][i] == 0)
+                ++emptySpot;
+        }
+        if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+        in_a_row = emptySpot = 0;
+    }
+    if (y + z == 2) { // if on the second diagonal
+        for (int i = 0; i < 3; i++) {
+            if (board[x][2-i][i] == 2)
+                ++in_a_row;
+            else if (board[x][2-i][i] == 0)
+                ++emptySpot;
+        }
+        if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+        in_a_row = emptySpot = 0;
+    }
+    // check set-up in the 4 xyz diagonals, if on those diagonals
+    if (x == y && x == z) { // if on the (0,0,0) to (2,2,2) diagonal
+        for (int i = 0; i < 3; i++) {
+            if (board[i][i][i] == 2)
+                ++in_a_row;
+            else if (board[i][i][i] == 0)
+                ++emptySpot;
+        }
+        if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+        in_a_row = emptySpot = 0;
+    }
+    if (x + y == 2 && y == z) { // if on the (2,0,0) to (0,2,2) diagonal
+        for (int i = 0; i < 3; i++) {
+            if (board[2-i][i][i] == 2)
+                ++in_a_row;
+            else if (board[2-i][i][i] == 0)
+                ++emptySpot;
+        }
+        if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+        in_a_row = emptySpot = 0;
+    }
+    if (x + y == 2 && x == z) { // if on the (0,2,0) to (2,0,2) diagonal
+        for (int i = 0; i < 3; i++) {
+            if (board[i][2-i][i] == 2)
+                ++in_a_row;
+            else if (board[i][2-i][i] == 0)
+                ++emptySpot;
+        }
+        if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+        in_a_row = emptySpot = 0;
+    }
+    if (x + z == 2 && x == y) { // if on the (0,0,2) to (2,2,0) diagonal
+        for (int i = 0; i < 3; i++) {
+            if (board[i][i][2-i] == 2)
+                ++in_a_row;
+            else if (board[i][i][2-i] == 0)
+                ++emptySpot;
+        }
+        if (in_a_row == 2 && emptySpot == 1) ++set_ups;
+    }
+    return set_ups;
 }
