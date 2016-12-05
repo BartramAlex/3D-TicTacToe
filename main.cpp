0e321e0ca75defefd18a01b09e0af93d7cfc9041
thehollowoak <thehollowoak@gmail.com>
2016-12-05 15:10:55 -0500

    Update main.cpp
    Modified for any size board

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
@@ -7,7 +6,9 @@
 
 using namespace std;
 
-int board[3][3][3] = {0};
+const int SIZE = 4;
+
+int board[SIZE*SIZE*SIZE] = {0};
 int x, y, z; // hold the last place played
 void displayBoard();
 void getPlayerMove();
@@ -15,7 +16,7 @@ void getAImove();
 bool validate();
 int checkWin();
 
-int main()
+int main(int argc, char *argv[])
 {
     srand (time(NULL));
     cout << "Welcome to 3D tic-tac-toe\n";
@@ -42,17 +43,18 @@ int main()
 
 
 void displayBoard() {
-    for (int i=2; i>-1; i--) {
-        for (int j=0; j<3; j++) {
-            for (int k=0; k<3; k++) {
-                if (board[k][j][i] == 1) cout << " X ";
-                else if (board[k][j][i] == 2) cout << " O ";
-                else cout << "   ";
-                if (k<2) cout << '|';
-            }
-            if (j<2) cout << endl << "---+---+---" << endl;
+    for (int i=0; i<SIZE*SIZE*SIZE; i++) {
+        if (board[i] == 1) cout << " X ";
+        else if (board[i] == 2) cout << " O ";
+        else cout << "   ";
+        if (i%SIZE<SIZE-1) cout << '|';
+        else if (i%(SIZE*SIZE)<SIZE*SIZE-1) {
+            cout << endl << "---";
+            for (int j=0; j<SIZE-1; j++)
+                cout << "+---";
+            cout << endl;
         }
-        cout << endl << endl;
+        else cout << endl << endl;
     }
     cout << "~~~~~~~~~~~~~~~~~~\n";
 }
@@ -67,174 +69,173 @@ void getPlayerMove() {
     }
     --x;
     --y;
-    if (board[x][y][0] == 0) {z = 0;}
-    else if (board[x][y][1] == 0) {z = 1;}
-    else {z = 2;}
-    board[x][y][z] = 1;
+    z = 0;
+    for (int i = SIZE-1; i>0; i--) {
+        if (board[x+SIZE*(y+SIZE*i)] == 0) {z = i; break;}
+    }
+    board[x+SIZE*(y+SIZE*z)] = 1;
 }
 
 bool validate() {
-    if (board[x-1][y-1][2] != 0) return false; // if spot all full, return false
-    return (x == 1 || x == 2 || x == 3) && (y == 1 || y == 2 || y == 3);
+    if (board[x-1+SIZE*(y-1)] != 0) return false; // if spot all full, return false
+    return (x > 0 && x <= SIZE) && (y > 0 && y <= SIZE);
 }
 
 void getAImove() {
     // get priorities for each spot and record the highest priority
     int p = 7; // which priority is highest
     int n = 0; // how many of that priority there is
-    int priorities[3][3];
+    int priorities[SIZE*SIZE];
 
     // time decisions
     clock_t startTime, endTime;
     startTime = clock();
-    
-# pragma omp parallel num_threads(3)
-    {
-        int i = omp_get_thread_num();
-        //int i = rank%3;
-        //int j = rank/3;
 
-    //for (int i=0; i<3; i++) { // replace with parallel code
-        for (int j=0; j<3; j++) {
-            priorities[i][j] = getPriority(i, j, board);
-            if (priorities[i][j] < p) {
-                p = priorities[i][j];
-                n = 1;
-            }
-            else if (priorities[i][j] == p) ++n;
+    # pragma omp parallel num_threads(9)
+    {
+        int rank = omp_get_thread_num();
+        int i = rank%3;
+        int j = rank/3;*/
+
+    for (int i=0; i<SIZE*SIZE; i++) { // replace with parallel code
+        priorities[i] = getPriority(i%SIZE, i/SIZE, board, SIZE);
+        //cout << "For " << i << ": p = " << priorities[i] << endl;
+        if (priorities[i] < p) {
+            p = priorities[i];
+            n = 1;
         }
+        else if (priorities[i] == p) ++n;
     }
-	#pragma omp barrier
+    #pragma omp barrier
     endTime = clock();
-	cout << "Time in parallel: " << (double)(endTime - startTime)/CLOCKS_PER_SEC << endl;
+    cout << "Time in parallel: " << (double)(endTime - startTime)/CLOCKS_PER_SEC << endl;
 
     int choice = rand() % n; // choose randomly between spots of the highest priority
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<3; j++) {
-            if (priorities[i][j] == p) {
-                if (choice == 0) {
-                    x = i;
-                    y = j;
-                    break;
-                }
-                else --choice;
+    for (int i=0; i<SIZE*SIZE; i++) {
+        if (priorities[i] == p) {
+            if (choice == 0) {
+                x = i%SIZE;
+                y = i/SIZE;
+                break;
             }
+            else --choice;
         }
     }
 
     // place choice on board;
-    if (board[x][y][0] == 0) {z = 0;}
-    else if (board[x][y][1] == 0) {z = 1;}
-    else {z = 2;}
-    board[x][y][z] = 2;
+    z = 0;
+    for (int i = SIZE-1; i>0; i--) {
+        if (board[x+SIZE*(y+SIZE*i)] == 0) {z = i; break;}
+    }
+    board[x+SIZE*(y+SIZE*z)] = 2;
 }
 
 int checkWin() { // returns 0 for no win, 1 for player, 2 for AI, (and 3 for board full?)
     int in_a_row = 0;
-    int winner = board[x][y][z]; // winner will be 1 or 2
+    int winner = board[x+SIZE*(y+SIZE*z)]; // winner will be 1 or 2
     // check win in x direction
-    for (int i = 0; i < 3; i++) {
-        if (board[i][y][z] == winner)
+    for (int i = 0; i < SIZE; i++) {
+        if (board[i+SIZE*(y+SIZE*z)] == winner)
             ++in_a_row;
     }
-    if (in_a_row == 3) return winner;
+    if (in_a_row == SIZE) return winner;
     // check win in y direction
     in_a_row = 0;
-    for (int i = 0; i < 3; i++) {
-        if (board[x][i][z] == winner)
+    for (int i = 0; i < SIZE; i++) {
+        if (board[x+SIZE*(i+SIZE*z)] == winner)
             ++in_a_row;
     }
-    if (in_a_row == 3) return winner;
+    if (in_a_row == SIZE) return winner;
     in_a_row = 0;
     // check win in y direction
-    for (int i = 0; i < 3; i++) {
-        if (board[x][y][i] == winner)
+    for (int i = 0; i < SIZE; i++) {
+        if (board[x+SIZE*(y+SIZE*i)] == winner)
             ++in_a_row;
     }
-    if (in_a_row == 3) return winner;
+    if (in_a_row == SIZE) return winner;
     in_a_row = 0;
     // check win in xy diagonals if spot is on that diagonal
     if (x == y) { // if on first diagonal
-        for (int i = 0; i < 3; i++) {
-            if (board[i][i][z] == winner)
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i+SIZE*(i+SIZE*z)] == winner)
                 ++in_a_row;
         }
-        if (in_a_row == 3) return winner;
+        if (in_a_row == SIZE) return winner;
         in_a_row = 0;
     }
-    if (x + y == 2) { // if on the second diagonal
-        for (int i = 0; i < 3; i++) {
-            if (board[i][2-i][z] == winner)
+    if (x + y == SIZE-1) { // if on the second diagonal
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i+SIZE*(2-i+SIZE*z)] == winner)
                 ++in_a_row;
         }
-        if (in_a_row == 3) return winner;
+        if (in_a_row == SIZE) return winner;
         in_a_row = 0;
     }
     // check win in xz diagonals if spot is on that diagonal
     if (x == z) { // if on first diagonal
-        for (int i = 0; i < 3; i++) {
-            if (board[i][y][i] == winner)
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i+SIZE*(y+SIZE*i)] == winner)
                 ++in_a_row;
         }
-        if (in_a_row == 3) return winner;
+        if (in_a_row == SIZE) return winner;
         in_a_row = 0;
     }
-    if (x + z == 2) { // if on the second diagonal
-        for (int i = 0; i < 3; i++) {
-            if (board[i][y][2-i] == winner)
+    if (x + z == SIZE-1) { // if on the second diagonal
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i+SIZE*(y+SIZE*(2-i))] == winner)
                 ++in_a_row;
         }
-        if (in_a_row == 3) return winner;
+        if (in_a_row == SIZE) return winner;
         in_a_row = 0;
     }
     // check win in yz diagonals if spot is on that diagonal
     if (y == z) { // if on first diagonal
         for (int i = 0; i < 3; i++) {
-            if (board[x][i][i] == winner)
+            if (board[x+SIZE*(i+SIZE*i)] == winner)
                 ++in_a_row;
         }
-        if (in_a_row == 3) return winner;
+        if (in_a_row == SIZE) return winner;
         in_a_row = 0;
     }
-    if (y + z == 2) { // if on the second diagonal
-        for (int i = 0; i < 3; i++) {
-            if (board[x][2-i][i] == winner)
+    if (y + z == SIZE-1) { // if on the second diagonal
+        for (int i = 0; i < SIZE; i++) {
+            if (board[x+SIZE*(2-i+SIZE*(2-i))] == winner)
                 ++in_a_row;
         }
-        if (in_a_row == 3) return winner;
+        if (in_a_row == SIZE) return winner;
         in_a_row = 0;
     }
     // check win in the 4 xyz diagonals, if on those diagonals
     if (x == y && x == z) { // if on the (0,0,0) to (2,2,2) diagonal
-        for (int i = 0; i < 3; i++) {
-            if (board[i][i][i] == winner)
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i+SIZE*(i+SIZE*i)] == winner)
                 ++in_a_row;
         }
-        if (in_a_row == 3) return winner;
+        if (in_a_row == SIZE) return winner;
         in_a_row = 0;
     }
-    if (x + y == 2 && y == z) { // if on the (2,0,0) to (0,2,2) diagonal
-        for (int i = 0; i < 3; i++) {
-            if (board[2-i][i][i] == winner)
+    if (x + y == SIZE-1 && y == z) { // if on the (2,0,0) to (0,2,2) diagonal
+        for (int i = 0; i < SIZE; i++) {
+            if (board[2-i+SIZE*(i+SIZE*i)] == winner)
                 ++in_a_row;
         }
-        if (in_a_row == 3) return winner;
+        if (in_a_row == SIZE) return winner;
         in_a_row = 0;
     }
-    if (x + y == 2 && x == z) { // if on the (0,2,0) to (2,0,2) diagonal
-        for (int i = 0; i < 3; i++) {
-            if (board[i][2-i][i] == winner)
+    if (x + y == SIZE-1 && x == z) { // if on the (0,2,0) to (2,0,2) diagonal
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i+SIZE*(2-i+SIZE*i)] == winner)
                 ++in_a_row;
         }
-        if (in_a_row == 3) return winner;
+        if (in_a_row == SIZE) return winner;
         in_a_row = 0;
     }
-    if (x + z == 2 && x == y) { // if on the (0,0,2) to (2,2,0) diagonal
-        for (int i = 0; i < 3; i++) {
-            if (board[i][i][2-i] == winner)
+    if (x + z == SIZE-1 && x == y) { // if on the (0,0,2) to (2,2,0) diagonal
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i+SIZE*(i+SIZE*(2-i))] == winner)
                 ++in_a_row;
         }
-        if (in_a_row == 3) return winner;
+        if (in_a_row == SIZE) return winner;
     }
     return 0;
 }
